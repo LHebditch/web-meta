eecffae6df31119c0dba152e2a34c46971028a32
Lyndon <lyndonhebditch@gmail.com>
2022-12-13 14:52:52 +0000

    use google bot user agent

--- handlers/webmeta/handler.go
+++ handlers/webmeta/handler.go
@@ -43,7 +43,7 @@ func CreateRequest(log *zap.Logger, url string) (req *http.Request, err error) {
 		log.Error("failed to create request", zap.Error(err))
 		return
 	}
-	req.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
+	req.Header.Set("user-agent", "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
 	req.Header.Set("accept", "text/html")
 	req.Header.Set("cache-control", "max-ag=0")
 	req.Header.Set("charset", "utf-8")
